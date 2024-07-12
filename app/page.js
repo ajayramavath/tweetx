be9d13b813d5aede56f115dffba38853018306f1
ajayramavath <ajayramavath03@gmail.com>
2024-07-12 07:13:14 +0530

    -

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ import { redirect } from 'next/navigation'
 
 export default async function Page(){
   const session = await getServerSession(authOptions)
-  console.log(session)
   if(!session){
     redirect('/auth/signin',"replace")
   }
